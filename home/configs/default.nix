7a09d74af98af6515d70376fdd796e06e05b1a91
joao-vitorg <101678877+joao-vitorg@users.noreply.github.com>
2025-10-11 18:23:32 -0300

    Adiciona konsave

--- home/configs/default.nix
+++ home/configs/default.nix
@@ -1,5 +1,9 @@
 { config, ... }: {
   xdg.configFile = {
+    "konsave/profiles/dallas" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/media/codes/nixos-config/home/configs/konsave";
+      recursive = true;
+    };
     "MangoHud" = {
       source = config.lib.file.mkOutOfStoreSymlink "/media/codes/nixos-config/home/configs/MangoHud";
       recursive = true;
--- home/packages/fish.nix
+++ home/packages/fish.nix
@@ -10,12 +10,13 @@
 
     functions = {
       nix-hardware = "doas nixos-generate-config \n cp /etc/nixos/hardware-configuration.nix /media/codes/nixos-config/hosts/laptop";
-      nix-update = "doas nix-channel --update";
-      nix-gc = "doas nh clean all \n doas nix-store --optimize";
-      nix-test = "nh os test -H dallas -u -- --impure";
-      nix-boot = "nh os boot -H dallas -u -- --impure";
+      nix-update = "nix flake update --allow-dirty \n doas nix-channel --update";
+      nix-gc = "doas nix-env --delete-generations old --profile /nix/var/nix/profiles/system \n doas nix-collect-garbage -d \n doas nix-store --optimize";
+      nix-test = "nixos-rebuild test --flake .#dallas --sudo";
+      nix-boot = "nixos-rebuild boot --flake .#dallas --sudo";
       nix-copilot = "ln -fs /run/current-system/sw/bin/copilot-agent ~/.local/share/JetBrains/IntelliJIdea2023.2/github-copilot-intellij/copilot-agent/bin/copilot-agent-linux";
       nix-upgrade = "nix-update \n nix-boot \n nix-gc";
+      nix-konsave = "konsave -f -s dallas";
     };
 
     plugins = with pkgs.fishPlugins; [
